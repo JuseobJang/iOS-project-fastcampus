81bd0102198da1d47535a92c6f6a1533d47eb215
JuseobJang <wntjq68@gmail.com>
2021-07-06 00:25:11 +0900

    feat: Create BountyList Project
    원피스 현상금 리스트 앱

---
+++ projects/BountyList/BountyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BountyList
+//
+//  Created by seob_jj on 2021/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
