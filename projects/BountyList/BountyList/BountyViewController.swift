a26979897fdcf95e0119d8e26ef7666372164446
JuseobJang <wntjq68@gmail.com>
2021-07-06 01:35:40 +0900

    feat: Configure UI
    1. 오토 레이아웃 적용
    2. 테이블 뷰와 셀을 이용한 화면구성
    3. 커스텀 셀 생성
    4. UITableViewDataSource, UITableViewDelegate 프로토콜 사용

---
+++ projects/BountyList/BountyList/BountyViewController.swift
@@ -0,0 +1,50 @@
+//
+//  BountyViewController.swift
+//  BountyList
+//
+//  Created by seob_jj on 2021/07/06.
+//
+
+import UIKit
+
+class BountyViewController: UIViewController
+                            , UITableViewDataSource, UITableViewDelegate {
+    
+    let nameList = ["brook","chopper","franky","luffy","nami","robin","sanji","zoro"]
+    let bountyList = [33000000,50,44000000,300000000,16000000,80000000,77000000,120000000]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    // UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bountyList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else{
+            return UITableViewCell()
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLabel.text = "\(bountyList[indexPath.row])"
+        return cell
+    }
+    
+    // UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--> \(indexPath.row)")
+    }
+}
+
+
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+}
