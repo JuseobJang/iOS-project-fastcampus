b16480e30b006b45bd2c7e65517110726c4fee56
JuseobJang <wntjq68@gmail.com>
2021-07-15 15:42:22 +0900

    feat: Change animation to using view properties
    Constraint를 이용한 애니메이션에서 view.transform을 이용하여 속성을 변화시키는 애니메이션으로 변경
    
    1. view.transform = CGAffineTransform() 을 사용하여 위치 크기 각도를 변경
    2. view.alpha를 사용하여 투명도 변경
    3. CGAffineTransform.identity 를 사용하여 원래 property로 되돌리기

--- projects/BountyList/BountyList/DetailViewController.swift
+++ projects/BountyList/BountyList/DetailViewController.swift
@@ -9,17 +9,17 @@
  MVVM pattern
  
  Model
-  - Bounty Info
-    BountyInfo 만들어야함
+ - Bounty Info
+ BountyInfo 만들어야함
  
  View
-  - imgView, nameLabel, bountyLabel
-    view들은 viewModel을 통해서 구성 되어야함
+ - imgView, nameLabel, bountyLabel
+ view들은 viewModel을 통해서 구성 되어야함
  
  ViewModel
-  - DetailViewModel
-    View에서 필요한 메서드 만들기
-    모델 가지고 있기 => BountyInfo
+ - DetailViewModel
+ View에서 필요한 메서드 만들기
+ 모델 가지고 있기 => BountyInfo
  */
 import UIKit
 
@@ -28,11 +28,11 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
-    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
-    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
+//    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+//    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
     
     let viewModel = DetailViewModel()
-
+    
     // 뷰가 보여지기 직전
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,23 +55,42 @@ class DetailViewController: UIViewController {
     }
     
     func prepareAnimation(){
-        nameLabelCenterX.constant = view.bounds.width
-        bountyLabelCenterX.constant = view.bounds.width
+        //        nameLabelCenterX.constant = view.bounds.width
+        //        bountyLabelCenterX.constant = view.bounds.width
+        
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
     }
     
     func showAnimation(){
-        nameLabelCenterX.constant = 0
-        bountyLabelCenterX.constant = 0
+        //        nameLabelCenterX.constant = 0
+        //        bountyLabelCenterX.constant = 0
         
-//        UIView.animate(withDuration: 0.3){
-//            self.view.layoutIfNeeded() // constant 값이 변경 되었으므로 layout을 다시 해줘야함 
-//        }
-//        UIView.animate(withDuration: 0.3, delay: 0.1, options: .curveEaseIn, animations: {self.view.layoutIfNeeded()}, completion: nil)
+        //        UIView.animate(withDuration: 0.3){
+        //            self.view.layoutIfNeeded() // constant 값이 변경 되었으므로 layout을 다시 해줘야함
+        //        }
+        //        UIView.animate(withDuration: 0.3, delay: 0.1, options: .curveEaseIn, animations: {self.view.layoutIfNeeded()}, completion: nil)
         
-        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {self.view.layoutIfNeeded()}, completion: nil)
+        //        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {self.view.layoutIfNeeded()}, completion: nil)
+        //
+        //        UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        
+        
+        // identity 로 transform 이전 속성을 가져 올 수 있음
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+            self.nameLabel.alpha = 1
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.alpha = 1
+        }, completion: nil)
         
         UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
-        
     }
     
     @IBAction func close(_ sender: Any) {
