3d3a2c8462ddc364924f77a35922e0e27e099522
JuseobJang <wntjq68@gmail.com>
2021-07-19 15:41:59 +0900

    feat: Implement SimplePlayer
    1. 싱글톤 으로 객체 생성
    2. AVPlayer를 사용하여 메서드 구현

--- projects/AppleMusicStApp/AppleMusicStApp/Player/SimplePlayer.swift
+++ projects/AppleMusicStApp/AppleMusicStApp/Player/SimplePlayer.swift
@@ -10,50 +10,50 @@ import AVFoundation
 
 class SimplePlayer {
     // TODO: 싱글톤 만들기, 왜 만드는가?
-    
+    static let shared = SimplePlayer()
     
     private let player = AVPlayer()
 
     var currentTime: Double {
         // TODO: currentTime 구하기
-        return 0.0
+        return player.currentItem?.currentTime().seconds ?? 0
     }
     
     var totalDurationTime: Double {
         // TODO: totalDurationTime 구하기
-        return 0.0
+        return player.currentItem?.duration.seconds ?? 0
     }
     
     var isPlaying: Bool {
         // TODO: isPlaying 구하기
-        return false
+        return player.isPlaying
     }
     
     var currentItem: AVPlayerItem? {
         // TODO: currentItem 구하기
-        return nil
+        return player.currentItem
     }
     
     init() { }
     
     func pause() {
         // TODO: pause구현
-        
+        player.pause()
     }
     
     func play() {
         // TODO: play구현
-        
+        player.play()
     }
     
     func seek(to time:CMTime) {
         // TODO: seek구현
-        
+        player.seek(to: time)
     }
     
     func replaceCurrentItem(with item: AVPlayerItem?) {
         // TODO: replace current item 구현
-        
+        player.replaceCurrentItem(with: item)
     }
     
     func addPeriodicTimeObserver(forInterval: CMTime, queue: DispatchQueue?, using: @escaping (CMTime) -> Void) {
