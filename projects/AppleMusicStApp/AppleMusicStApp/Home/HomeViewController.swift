ceaffab4f909ce28fdfb75019471801dc4c0e4ac
JuseobJang <wntjq68@gmail.com>
2021-07-19 15:42:42 +0900

    feat: Connect HomeView and PlayerView
    1. PlayerStoryBoard를 받아와 해당 스토리 보드로 PlayerView에 해당 아이템으로 정보 초기화 후 present()함수로 화면 띄우기

--- projects/AppleMusicStApp/AppleMusicStApp/Home/HomeViewController.swift
+++ projects/AppleMusicStApp/AppleMusicStApp/Home/HomeViewController.swift
@@ -51,7 +51,11 @@ extension HomeViewController: UICollectionViewDataSource {
 
             header.update(with: item)
             header.tapHandler = { item -> Void in
-                print("item title : \(item.convertToTrack()?.title)")
+                let playerStoryBoard = UIStoryboard.init(name: "Player", bundle: nil)
+                guard let playerVC = playerStoryBoard.instantiateViewController(identifier: "PlayerViewController") as? PlayerViewController
+                else { return }
+                playerVC.simplePlayer.replaceCurrentItem(with: item)
+                self.present(playerVC, animated: true, completion: nil)
             }
             
             return header
@@ -65,6 +69,14 @@ extension HomeViewController: UICollectionViewDelegate {
     // 클릭했을때 어떻게 할까?
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // TODO: 곡 클릭시 플레이어뷰 띄우기
+        let playerStoryBoard = UIStoryboard.init(name: "Player", bundle: nil)
+        guard let playerVC = playerStoryBoard.instantiateViewController(identifier: "PlayerViewController") as? PlayerViewController
+        else { return }
+        
+        let item = trackManager.tracks[indexPath.item]
+        playerVC.simplePlayer.replaceCurrentItem(with: item)
+        present(playerVC, animated: true, completion: nil)
+        
     }
 }
 
