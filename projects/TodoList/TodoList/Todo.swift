9343e7fc8e697cab37c1632a2248db3b8e8b4c29
JuseobJang <wntjq68@gmail.com>
2021-07-20 16:12:09 +0900

    feat: Update TodoManager
    Todo Struct를 관리하는 역할
    1. createTodo(), updateTodo(),  deleteTodo(), addTodo() 구현
    2. 각 메서드 실행 마지막에 saveTodo()를 통해 json으로 저장

--- projects/TodoList/TodoList/Todo.swift
+++ projects/TodoList/TodoList/Todo.swift
@@ -31,6 +31,7 @@ struct Todo: Codable, Equatable {
 
 class TodoManager {
     
+    // Sigleton 객체 사용
     static let shared = TodoManager()
     
     static var lastId: Int = 0
@@ -39,21 +40,31 @@ class TodoManager {
     
     func createTodo(detail: String, isToday: Bool) -> Todo {
         //TODO: create로직 추가
-        return Todo(id: 1, isDone: false, detail: "2", isToday: true)
+        
+        let nextId = TodoManager.lastId + 1
+        TodoManager.lastId = nextId
+        return Todo(id: nextId, isDone: false, detail: detail, isToday: isToday)
     }
     
     func addTodo(_ todo: Todo) {
         //TODO: add로직 추가
+        todos.append(todo)
+        saveTodo()
     }
     
     func deleteTodo(_ todo: Todo) {
         //TODO: delete 로직 추가
-        
+        if let index = todos.firstIndex(of: todo) {
+            todos.remove(at: index)
+            saveTodo()
+        }
     }
     
     func updateTodo(_ todo: Todo) {
         //TODO: updatee 로직 추가
-        
+        guard let index = todos.firstIndex(of: todo) else { return }
+        todos[index].update(isDone: todo.isDone, detail: todo.detail, isToday: todo.isToday)
+        saveTodo()
     }
     
     func saveTodo() {
