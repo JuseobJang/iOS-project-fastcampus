1b07c48ecdad56d870ee5092d8dee89be6a78c20
JuseobJang <wntjq68@gmail.com>
2021-07-20 14:42:59 +0900

    feat: Create TodoList project

---
+++ projects/TodoList/TodoList/Todo.swift
@@ -0,0 +1,117 @@
+//
+//  Todo.swift
+//  TodoList
+//
+//  Created by joonwon lee on 2020/03/19.
+//  Copyright © 2020 com.joonwon. All rights reserved.
+//
+
+import UIKit
+
+
+// TODO: Codable과 Equatable 추가
+struct Todo: Codable, Equatable {
+    let id: Int
+    var isDone: Bool
+    var detail: String
+    var isToday: Bool
+    
+    mutating func update(isDone: Bool, detail: String, isToday: Bool) {
+        // TODO: update 로직 추가
+        
+    }
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        // TODO: 동등 조건 추가
+        return true
+    }
+}
+
+class TodoManager {
+    
+    static let shared = TodoManager()
+    
+    static var lastId: Int = 0
+    
+    var todos: [Todo] = []
+    
+    func createTodo(detail: String, isToday: Bool) -> Todo {
+        //TODO: create로직 추가
+        return Todo(id: 1, isDone: false, detail: "2", isToday: true)
+    }
+    
+    func addTodo(_ todo: Todo) {
+        //TODO: add로직 추가
+    }
+    
+    func deleteTodo(_ todo: Todo) {
+        //TODO: delete 로직 추가
+        
+    }
+    
+    func updateTodo(_ todo: Todo) {
+        //TODO: updatee 로직 추가
+        
+    }
+    
+    func saveTodo() {
+        Storage.store(todos, to: .documents, as: "todos.json")
+    }
+    
+    func retrieveTodo() {
+        todos = Storage.retrive("todos.json", from: .documents, as: [Todo].self) ?? []
+        
+        let lastId = todos.last?.id ?? 0
+        TodoManager.lastId = lastId
+    }
+}
+
+class TodoViewModel {
+    
+    enum Section: Int, CaseIterable {
+        case today
+        case upcoming
+        
+        var title: String {
+            switch self {
+            case .today: return "Today"
+            default: return "Upcoming"
+            }
+        }
+    }
+    
+    private let manager = TodoManager.shared
+    
+    var todos: [Todo] {
+        return manager.todos
+    }
+    
+    var todayTodos: [Todo] {
+        return todos.filter { $0.isToday == true }
+    }
+    
+    var upcompingTodos: [Todo] {
+        return todos.filter { $0.isToday == false }
+    }
+    
+    var numOfSection: Int {
+        return Section.allCases.count
+    }
+    
+    func addTodo(_ todo: Todo) {
+        manager.addTodo(todo)
+    }
+    
+    func deleteTodo(_ todo: Todo) {
+        manager.deleteTodo(todo)
+    }
+    
+    func updateTodo(_ todo: Todo) {
+        manager.updateTodo(todo)
+    }
+    
+    func loadTasks() {
+        manager.retrieveTodo()
+    }
+}
+
