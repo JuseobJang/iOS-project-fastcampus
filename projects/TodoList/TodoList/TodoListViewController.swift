09f2ed63daa4aaa66fde5ecf490138254ae82ed6
JuseobJang <wntjq68@gmail.com>
2021-07-21 14:44:29 +0900

    feat: Update TodoListCell
    1. updateUI() : 전달 받은 todo를 통해 UI를 업데이트
    2. reset() : cell 을 원래 초기상태로 돌려줌
    3. checkButtonTapped() : 현재 체크버튼의 상태를 통해 눌렸을 시 각각 반대 상태로 바꿔줌
    
    doneButtonTapHandler, deleteButtonTapHandler
    : 실제 데이터를 처리 해줄것이라고 명시만 하고 실제 비즈니스 로직은 해당 셀을 선언한 부분에서 구현 할 수 있도록 한다.

--- projects/TodoList/TodoList/TodoListViewController.swift
+++ projects/TodoList/TodoList/TodoListViewController.swift
@@ -159,13 +159,20 @@ class TodoListCell: UICollectionViewCell {
     
     func reset() {
         // TODO: reset로직 구현
-        
+        descriptionLabel.alpha = 1
+        deleteButton.isHidden = true
+        showStrikeThrough(false)
     }
     
     @IBAction func checkButtonTapped(_ sender: Any) {
         // TODO: checkButton 처리
+        checkButton.isSelected = !checkButton.isSelected
+        let isDone = checkButton.isSelected
+        showStrikeThrough(isDone)
+        descriptionLabel.alpha = isDone ? 0.2 : 1
+        deleteButton.isHidden = !isDone
         
-
+        doneButtonTapHandler?(isDone) // 실제 데이터에 대한 처리
     }
     
     @IBAction func deleteButtonTapped(_ sender: Any) {
