45f27a07149225dc5462fce3371f014ee5258524
JuseobJang <wntjq68@gmail.com>
2021-07-23 13:48:55 +0900

    feat: Create MyNetflix project
    MyNetflix project 시작

---
+++ projects/MyNetflix/MyNetflix/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  MyNetflix
+//
+//  Created by joonwon lee on 2020/04/02.
+//  Copyright © 2020 com.joonwon. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
