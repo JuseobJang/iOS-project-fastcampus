a562717198c72ddabc523f0bededc74152a126b0
JuseobJang <wntjq68@gmail.com>
2021-07-23 15:26:14 +0900

    feat: Implement parsing response
    1. Response struct를 codable을 활용 하여 구현
    2. Moive struct를 cadable을 활용 하여 구현
    3. JSONDecoder를 이용하여 http response를 parse

--- projects/MyNetflix/MyNetflix/SearchViewController.swift
+++ projects/MyNetflix/MyNetflix/SearchViewController.swift
@@ -38,6 +38,7 @@ extension SearchViewController: UISearchBarDelegate {
         
         SearchApi.search(searchTerm){ movies in
             // collection view 로 표현
+            print("---> \(movies.count) + \(movies.first!.title)")
             
         }
         
@@ -77,25 +78,47 @@ class SearchApi{
                 completion([])
                 return
             }
-            
-            let str = String(data: resultData, encoding: .utf8)
-            print(str)
-            
-//            completion([Movie])
-            
+            let movies = parseMovies(resultData)
+            completion(movies)
         }
         dataTask.resume()
+    }
+    
+    static func parseMovies(_ data: Data) -> [Movie] {
+        let decoder = JSONDecoder()
         
-        
+        do {
+            let response = try decoder.decode(Response.self, from: data)
+            let movies = response.movies
+            return movies
+        } catch let error {
+            print("---> parsing error: \(error.localizedDescription)")
+            return []
+        }
     }
     
 }
 
-struct Response{
+struct Response: Codable {
+    let resultCount: Int
+    let movies: [Movie]
     
-    
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case movies = "results"
+    }
 }
 
-struct Movie {
+struct Movie: Codable {
+    let title: String
+    let director: String
+    let thumbnailPath: String
+    let previewURL: String
     
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case director = "artistName"
+        case thumbnailPath = "artworkUrl100"
+        case previewURL = "previewUrl"
+    }
 }
