32bf03f218b08105f9410c5460b408466ad5ad09
JuseobJang <wntjq68@gmail.com>
2021-07-23 14:08:13 +0900

    feat: Update SearchBar
    1. SearchViewController에 SearchBar & Collection 추가 
    2. SearchBar 의 'search'클릭시 이벤트 처리
     - 키보드 내리기
     - 빈값 확인

--- projects/MyNetflix/MyNetflix/SearchViewController.swift
+++ projects/MyNetflix/MyNetflix/SearchViewController.swift
@@ -10,8 +10,34 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    // 검색 버튼 눌렸을 때 처리 구현
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        // 키보드가 올라와 있을떄, 내려가게 처리
+        dismissKeyboard()
+        
+        // 검색어가 있는 지 확인
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else {
+            return
+        }
+        
+        
+        // 네트워킹을 통한 검색
+        
+        print("---> \(searchBar.text)")
+    }
+    
+    private func dismissKeyboard(){
+        searchBar.resignFirstResponder()
     }
     
 }
