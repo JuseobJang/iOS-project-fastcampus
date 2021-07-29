7b83415ad6870c9479006165d022a9c9d8d098ce
JuseobJang <wntjq68@gmail.com>
2021-07-29 14:53:09 +0900

    feat: Create Firebase project
    1. Firebase 연동
     - Analytics
     - Realtime Database

---
+++ projects/Firebase101/Firebase101/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase101
+//
+//  Created by seob_jj on 2021/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
