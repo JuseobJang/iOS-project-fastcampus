ef15e838fc2a402c7fc4f4222ceb0a17481e345d
JuseobJang <wntjq68@gmail.com>
2021-07-29 15:25:17 +0900

    feat: Load firebase realtime database
    1. realtime database 에 있는 예제 데이터 로딩 후 라벨로 표시

--- projects/Firebase101/Firebase101/ViewController.swift
+++ projects/Firebase101/Firebase101/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    let db = Database.database().reference()
+    @IBOutlet weak var dataLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateLabel()
         // Do any additional setup after loading the view.
     }
-
-
+    func updateLabel(){
+        db.child("firstData").observeSingleEvent(of: .value){ snapshot in
+            print("\(snapshot)")
+            let value = snapshot.value as? String ?? ""
+            
+            DispatchQueue.main.async {
+                self.dataLabel.text = value
+            }
+        }
+    }
 }
 
