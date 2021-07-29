c2f49d151493471499c50e80e5202d3619ea92c1
JuseobJang <wntjq68@gmail.com>
2021-07-29 15:53:31 +0900

    feat: Save data in firebase realtime database
    1. 여러가지 기본 타입 데이터 realtime database에 저장
    2. Customer, Book struct를 dIct로 변환하여 realtime database에 저장

--- projects/Firebase101/Firebase101/ViewController.swift
+++ projects/Firebase101/Firebase101/ViewController.swift
@@ -16,7 +16,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateLabel()
-        // Do any additional setup after loading the view.
+        saveBasicTypes()
+        saveCustomers()
+        
     }
     func updateLabel(){
         db.child("firstData").observeSingleEvent(of: .value){ snapshot in
@@ -30,3 +32,54 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController {
+    func saveBasicTypes(){
+        // Firebase child("key").setValue(Value)
+        // - string, number, dict, array
+        db.child("int").setValue(3);
+        db.child("double").setValue(3.5)
+        db.child("str").setValue("hihi")
+        db.child("array").setValue(["a","b","c"])
+        db.child("dict").setValue(["id": "anyId", "age": 10, "city": "seoul"])
+    }
+    
+    func saveCustomers(){
+        let books = [Book(title: "해리포터와 불의 잔", author: "J.K.롤링"),
+                     Book(title: "해리포터와 마법사의 돌", author: "J.K.롤링")]
+        let newCustomer1 = Customer(id: "\(Customer.id)", name: "Jang", books: books)
+        Customer.id += 1
+        let newCustomer2 = Customer(id: "\(Customer.id)", name: "Kang", books: books)
+        Customer.id += 1
+        let newCustomer3 = Customer(id: "\(Customer.id)", name: "Bae", books: books)
+        Customer.id += 1
+        
+        db.child("customers").child(newCustomer1.id).setValue(newCustomer1.toDict)
+        db.child("customers").child(newCustomer2.id).setValue(newCustomer2.toDict)
+        db.child("customers").child(newCustomer3.id).setValue(newCustomer3.toDict)
+    }
+}
+
+struct Customer{
+    static var id = 0
+    
+    let id: String
+    let name: String
+    let books: [Book]
+    
+    var toDict: [String: Any] {
+        let booksArray = books.map{$0.toDict}
+        let dict: [String: Any] = ["id": id, "name": name, "books": booksArray]
+        return dict
+    }
+}
+
+struct Book {
+    let title: String
+    let author: String
+    
+    var toDict: [String: Any]{
+        let dict: [String: Any] = ["title": title, "author": author]
+        return dict
+    }
+    
+}
