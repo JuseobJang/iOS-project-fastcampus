31764a3883f306be7ac8a7b77a7f50eda834b3e9
JuseobJang <wntjq68@gmail.com>
2021-08-01 14:58:33 +0900

    feat: Create FullScreenCamera project

---
+++ projects/FullScreenCamera/FullScreenCamera/CameraViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  FullScreenCamera
+//
+//  Created by joonwon lee on 28/04/2019.
+//  Copyright © 2019 com.joonwon. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Photos
+
+class CameraViewController: UIViewController {
+    // TODO: 초기 설정 1
+    
+
+    @IBOutlet weak var photoLibraryButton: UIButton!
+    @IBOutlet weak var previewView: PreviewView!
+    @IBOutlet weak var captureButton: UIButton!
+    @IBOutlet weak var blurBGView: UIVisualEffectView!
+    @IBOutlet weak var switchButton: UIButton!
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // TODO: 초기 설정 2
+        
+        
+    }
+    
+    func setupUI() {
+
+    }
+    
+    
+    @IBAction func switchCamera(sender: Any) {
+        // TODO: 카메라는 1개 이상이어야함
+        
+        
+        // TODO: 반대 카메라 찾아서 재설정
+        
+    }
+    
+    func updateSwitchCameraIcon(position: AVCaptureDevice.Position) {
+        // TODO: Update ICON
+        
+        
+    }
+    
+    @IBAction func capturePhoto(_ sender: UIButton) {
+        // TODO: photoOutput의 capturePhoto 메소드
+
+
+    }
+    
+    
+    func savePhotoLibrary(image: UIImage) {
+        // TODO: capture한 이미지 포토라이브러리에 저장
+    }
+}
+
+
+extension CameraViewController {
+    // MARK: - Setup session and preview
+    func setupSession() {
+        // TODO: captureSession 구성하기
+        // - presetSetting 하기
+        // - beginConfiguration
+        // - Add Video Input
+        // - Add Photo Output
+        // - commitConfiguration
+        
+
+        
+        
+    }
+    
+    
+    
+    func startSession() {
+        // TODO: session Start
+
+    }
+    
+    func stopSession() {
+        // TODO: session Stop
+        
+    }
+}
+
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        // TODO: capturePhoto delegate method 구현
+        
+        
+    }
+}
