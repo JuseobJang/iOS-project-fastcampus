66d212c48cb5450afbdfc315fa9579f60a2f3d1e
JuseobJang <wntjq68@gmail.com>
2021-09-04 16:06:10 +0900

    feat: Implement operation
    값입력 후 각 계산 버튼을 눌렀을 때 enum 을 활용하여 연산 기능을 분기 
    firstOperand와 secondOperand을 연산하여 결과를 구하고 결과를 다시 firstOperand에 저장 및 화면에 표시한다.

--- projects/Calcurator/Calcurator/ViewController.swift
+++ projects/Calcurator/Calcurator/ViewController.swift
@@ -52,14 +52,57 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func tapDivideButton(_ sender: UIButton) {
+        self.operation(.Divide)
     }
     @IBAction func tapMultiplyButton(_ sender: UIButton) {
+        self.operation(.Multiply)
     }
     @IBAction func tapAddButton(_ sender: UIButton) {
+        self.operation(.Add)
     }
     @IBAction func tapSubtractButton(_ sender: UIButton) {
+        self.operation(.Subtract)
     }
     @IBAction func tapEqualButton(_ sender: UIButton) {
+        self.operation(self.currentOperation)
+    }
+    
+    func operation(_ operation: Operation){
+        if self.currentOperation != .unknown {
+            if !self.displayNumber.isEmpty {
+                self.secondOperand = self.displayNumber
+                self.displayNumber = ""
+                
+                guard let firstOperand = Double(self.firstOperand) else { return }
+                guard let secondOperand = Double(self.secondOperand) else { return }
+                
+                switch self.currentOperation {
+                case .Add:
+                    self.result = "\(firstOperand + secondOperand)"
+                case .Subtract:
+                    self.result = "\(firstOperand - secondOperand)"
+                case .Divide:
+                    self.result = "\(firstOperand / secondOperand)"
+                case .Multiply:
+                    self.result = "\(firstOperand * secondOperand)"
+                default:
+                    break
+                }
+                if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0 {
+                    self.result = "\(Int(result))"
+                }
+                
+                self.firstOperand = self.result
+                self.numberOutputLabel.text = self.result
+            }
+            self.currentOperation = operation
+            
+        } else {
+            self.firstOperand = self.displayNumber
+            self.currentOperation = operation
+            self.displayNumber = ""
+        }
+        
     }
 }
 
