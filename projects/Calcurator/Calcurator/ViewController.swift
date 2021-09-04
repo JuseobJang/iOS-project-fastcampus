c524f2e5495b023c475b33386e10c1f02bde9280
JuseobJang <wntjq68@gmail.com>
2021-09-04 15:46:11 +0900

    feat: Implement Tap Button
    1. 숫자 버튼 클릭시 계산기에 숫자 추가(9자리 까지)
    2. AC 버튼 클릭시 모든 숫자 지우기
    3. "." 버튼 클릭시 조건을 확인하고 "." 추가

--- projects/Calcurator/Calcurator/ViewController.swift
+++ projects/Calcurator/Calcurator/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
+enum Operation {
+    case Add
+    case Subtract
+    case Divide
+    case Multiply
+    case unknown
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    var result = ""
+    var currentOperation: Operation = .unknown
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        guard let numberValue = sender.title(for: .normal) else { return }
+        if self.displayNumber.count < 9 {
+            self.displayNumber += numberValue
+            self.numberOutputLabel.text = self.displayNumber
+        }
+    }
+    @IBAction func tapClearButton(_ sender: UIButton) {
+        self.displayNumber = ""
+        self.firstOperand = ""
+        self.secondOperand = ""
+        self.result = ""
+        self.currentOperation = .unknown
+        self.numberOutputLabel.text = "0"
+    }
+    @IBAction func tapDotButton(_ sender: UIButton) {
+        if self.displayNumber.count < 8, !self.displayNumber.contains(".") {
+            self.displayNumber += self.displayNumber.isEmpty ? "0." : "."
+            self.numberOutputLabel.text = self.displayNumber
+        }
+    }
+    @IBAction func tapDivideButton(_ sender: UIButton) {
+    }
+    @IBAction func tapMultiplyButton(_ sender: UIButton) {
+    }
+    @IBAction func tapAddButton(_ sender: UIButton) {
+    }
+    @IBAction func tapSubtractButton(_ sender: UIButton) {
+    }
+    @IBAction func tapEqualButton(_ sender: UIButton) {
+    }
 }
 
