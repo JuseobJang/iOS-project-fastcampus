b09a85bb4078e2bd44eeeb22bbf5ad45a267fe56
JuseobJang <wntjq68@gmail.com>
2021-09-05 16:36:49 +0900

    feat: Implement login with google account
    1. 파이어베이스에서 구글 로그인 설정
    2. cocoaPod 을 사용해 GoogleSignIn 가져오기
    3. REVERSED_CLIENT_ID ; URL Scheme 에 등록
    4. AppDelegate가 GIDSignInDelegate 채택 후 GIDSignIn.sharedInstance() 초기 설정
    5 .func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool 구현
    6. 구글 로그인 버튼을 GIDSignInButton 으로 변경
    7. GIDSignIn.sharedInstance().presentingViewController 를 LoginViewController 로 설정하여 웹뷰가 보여질 컨트롤러 정하기
    8. 버튼을 탭하면 로그인 동작 실행
    9. 실제 구글 로그인 Authentication 관련 동작은 AppDelegate의 sign() 함수에 정의
    10. 성공시 MainViewController로 이동

--- projects/SpotifyLoginStApp/SpotifyLoginStApp/AppDelegate.swift
+++ projects/SpotifyLoginStApp/SpotifyLoginStApp/AppDelegate.swift
@@ -7,20 +7,29 @@
 
 import UIKit
 import Firebase
+import GoogleSignIn
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         // Firebase 초기화
         FirebaseApp.configure()
         
+        // GIDSignIn 초기 설정
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
+            
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url)
+    }
+
 
     // MARK: UISceneSession Lifecycle
 
@@ -34,6 +43,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    
+    // google 로그인
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print("ERROR Google Sign In \(error.localizedDescription)")
+            return
+        }
+        
+        guard let authentication = user.authentication else {return}
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        
+        Auth.auth().signIn(with: credential) { _, _ in
+            self.showMainViewController()
+        }
+        
+    }
+    
+    // 로그인 성공시 MainViewController 로 이동
+    private func showMainViewController(){
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let mainViewController = storyboard.instantiateViewController(identifier: "MainViewController")
+        mainViewController.modalPresentationStyle = .fullScreen
+        UIApplication.shared.windows.first?.rootViewController?.show(mainViewController, sender: nil)
     }
 
 
--- projects/SpotifyLoginStApp/SpotifyLoginStApp/LoginViewController.swift
+++ projects/SpotifyLoginStApp/SpotifyLoginStApp/LoginViewController.swift
@@ -6,11 +6,12 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailLoginButton: UIButton!
-    @IBOutlet weak var googleLoginButton: UIButton!
+    @IBOutlet weak var googleLoginButton: GIDSignInButton!
     @IBOutlet weak var appleLoginButton: UIButton!
     
     override func viewDidLoad() {
@@ -27,8 +28,12 @@ class LoginViewController: UIViewController {
         
         // 네비게이션 바 숨기기
         navigationController?.navigationBar.isHidden = true
+        
+        // Google Sign In
+        GIDSignIn.sharedInstance().presentingViewController = self
     }
     @IBAction func googleLoginButtonTapped(_ sender: UIButton) {
+        GIDSignIn.sharedInstance().signIn()
     }
     @IBAction func appleLoginButtonTapped(_ sender: UIButton) {
     }
