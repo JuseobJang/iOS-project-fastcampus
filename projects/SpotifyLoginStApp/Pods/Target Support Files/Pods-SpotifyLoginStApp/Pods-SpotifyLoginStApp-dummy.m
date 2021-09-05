c7bf34f409ed396edc78efa9b9878e4d642155c9
JuseobJang <wntjq68@gmail.com>
2021-09-05 15:13:34 +0900

    feat: Connect Firebase
    1. CocoaPod 을 이용하여 파이어베이스와 연동 및 초기화

---
+++ projects/SpotifyLoginStApp/Pods/Target Support Files/Pods-SpotifyLoginStApp/Pods-SpotifyLoginStApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpotifyLoginStApp : NSObject
+@end
+@implementation PodsDummy_Pods_SpotifyLoginStApp
+@end
--- projects/SpotifyLoginStApp/SpotifyLoginStApp/AppDelegate.swift
+++ projects/SpotifyLoginStApp/SpotifyLoginStApp/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Firebase 초기화
+        FirebaseApp.configure()
+        
         return true
     }
 
