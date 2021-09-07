1a6a03af44981d8a276b91d79b77dbae0f2b8e60
JuseobJang <wntjq68@gmail.com>
2021-09-07 17:43:12 +0900

    feat: Construct CardListCell UI
    1. CreditCard, PromotionDetail struct 정의
    2. CardListCell.xib 구성
    3. Main.storyboard 에 CardListCell 적용
    4. CardListViewController 에서 셀의 갯수, 셀의 표현, 셀의 높이 정의하기
    5. 셀의 표현에서 url을 사용하여 이미지를 띄우기 위해 Kingfisher 라이브러리 코코아팟을 사용하여 import 하기

---
+++ projects/CreditCardList/CreditCardList/CardListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CardListViewController.swift
+//  CreditCardList
+//
+//  Created by seob_jj on 2021/09/07.
+//
+
+import UIKit
+import Kingfisher
+
+class CardListViewController: UITableViewController {
+    
+    var creditCardList: [CreditCard] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // UITableView Cell Register
+        let nibName = UINib(nibName: "CardListCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return creditCardList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCell", for: indexPath) as? CardListCell  else {
+            return UITableViewCell()
+        }
+        
+        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
+        cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
+        
+        let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
+        cell.cardImageView.kf.setImage(with: imageURL)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+}
---
+++ projects/CreditCardList/CreditCardList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreditCardList
+//
+//  Created by seob_jj on 2021/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
