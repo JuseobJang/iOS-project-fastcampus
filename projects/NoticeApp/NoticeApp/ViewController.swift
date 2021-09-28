a6894971b1ccc6b541960f2f375a568710ed4a64
JuseobJang <wntjq68@gmail.com>
2021-09-28 16:02:52 +0900

    feat: Create NoticeApp Project
    Firebase를 통한 remote config 와 A/B test를 학습하기 위한 프로젝트

---
+++ projects/NoticeApp/NoticeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoticeApp
+//
+//  Created by seob_jj on 2021/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
