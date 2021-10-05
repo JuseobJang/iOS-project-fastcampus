ae7e56deb86ae2fd3dfbb327d847bba3d207f78e
JuseobJang <wntjq68@gmail.com>
2021-10-05 21:59:50 +0900

    feat: Implement notification trigger
    Alert 구조체의 date와  Calendar.current.dateComponents() 를 사용하여 시간과 분에 대한 컴포넌트 생성 및 이를 이용하여 트리거 만들기

--- projects/DrinkApp/DrinkApp/UNNotificationCenter.swift
+++ projects/DrinkApp/DrinkApp/UNNotificationCenter.swift
@@ -15,5 +15,10 @@ extension UNUserNotificationCenter{
         content.body = "세계보건기주(WHO)가 권장하는 하루 물 섭취량은 1.5L ~ 2L 입니다."
         content.sound = .default
         content.badge = 1
+        
+        
+        let component = Calendar.current.dateComponents([.hour, .minute], from: alert.date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: component, repeats: alert.isOn)
+        
     }
 }
