e2d72282585f7ee9b4bc7b14cb192a0998248c76
JuseobJang <wntjq68@gmail.com>
2021-10-07 13:45:59 +0900

    feat: Implement adding Todo(Task)
    Alert 와 테이블뷰 데이터 소스를 구현하여 투두리스트에 할 일 추가하기

---
+++ projects/TodoList2/TodoList2/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  TodoList2
+//
+//  Created by seob_jj on 2021/10/07.
+//
+
+import Foundation
+
+struct Task{
+    var title: String
+    var done: Bool
+}
--- projects/TodoList2/TodoList2/ViewController.swift
+++ projects/TodoList2/TodoList2/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var tasks = [Task]()
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
 
 
+    @IBAction func editButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
+            guard let title = alert.textFields?[0].text else {return}
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
+        })
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(registerButton)
+        alert.addAction(cancelButton)
+        alert.addTextField(configurationHandler: {textField in
+            textField.placeholder = "할 일을 입력해주세요."
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
