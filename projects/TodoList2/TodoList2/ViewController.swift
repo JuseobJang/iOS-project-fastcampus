c95e7846f04d5db71115a51cbdb64ead6104c68c
JuseobJang <wntjq68@gmail.com>
2021-10-07 13:21:15 +0900

    feat: Create TodoList2 project
    테이블 뷰를 이용한 투두리스트 앱

---
+++ projects/TodoList2/TodoList2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList2
+//
+//  Created by seob_jj on 2021/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
