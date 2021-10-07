e15454adc9ca096f255e21444c312d315cc03d97
JuseobJang <wntjq68@gmail.com>
2021-10-07 12:39:36 +0900

    feat: Create WarningApp project
    Firebase Cloud Messaging 을 이용한 재난문자 푸시알림 앱 프로젝트

---
+++ projects/WarningApp/WarningApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarningApp
+//
+//  Created by seob_jj on 2021/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
