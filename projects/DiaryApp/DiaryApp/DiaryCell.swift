9d93a8746c27ff312b72ad4261b72b22cd478fc9
JuseobJang <wntjq68@gmail.com>
2021-10-08 16:07:07 +0900

    feat: Construct diary app base UI
    1. 탭바를 이용하여 다이어리 뷰와 즐겨찾기 뷰로 나눔
    2. 콜렉션뷰와 콜렉션뷰 셀 기본 UI 구성

---
+++ projects/DiaryApp/DiaryApp/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  DiaryApp
+//
+//  Created by seob_jj on 2021/10/08.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ projects/DiaryApp/DiaryApp/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  DiaryApp
+//
+//  Created by seob_jj on 2021/10/08.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
