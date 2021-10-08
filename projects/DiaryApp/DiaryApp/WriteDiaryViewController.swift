3b068d8029e1dea102df38062cdff81652a0b539
JuseobJang <wntjq68@gmail.com>
2021-10-08 16:14:29 +0900

    feat: Construct WriteDiaryViewController UI
    다이어리 추가 뷰컨트롤러 UI 구성

---
+++ projects/DiaryApp/DiaryApp/WriteDiaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WriteDiaryViewController.swift
+//  DiaryApp
+//
+//  Created by seob_jj on 2021/10/08.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
