06dcc581eb13efb448becad1da08cc292c3bfc42
JuseobJang <wntjq68@gmail.com>
2021-10-08 15:18:41 +0900

    feat: Create DiaryApp project
    탭바와 콜렉션뷰 복습

---
+++ projects/DiaryApp/DiaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiaryApp
+//
+//  Created by seob_jj on 2021/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
