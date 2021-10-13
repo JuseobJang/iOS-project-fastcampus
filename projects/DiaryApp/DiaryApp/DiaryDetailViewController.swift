79e6e0018d043fc64d598179717972ed1f7a5dee
JuseobJang <wntjq68@gmail.com>
2021-10-13 14:30:48 +0900

    feat: Implement delete diary
    1. ViewController에서 아이템 선택시 DetailViewController로 이동 구현
    2. DetailViewController에서 아이템 삭제시 delegate를 이용하여 아이템 삭제

--- projects/DiaryApp/DiaryApp/DiaryDetailViewController.swift
+++ projects/DiaryApp/DiaryApp/DiaryDetailViewController.swift
@@ -7,23 +7,47 @@
 
 import UIKit
 
+protocol DiaryDetailViewDelegate: AnyObject {
+    func didSelectDelete(indexPath: IndexPath)
+}
+
 class DiaryDetailViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    weak var delegate: DiaryDetailViewDelegate?
+    
+    var diary: Diary?
+    var indexPath: IndexPath?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureView()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureView() {
+        guard let diary = self.diary else { return }
+        self.titleLabel.text = diary.title
+        self.contentsTextView.text = diary.contents
+        self.dateLabel.text = self.dateToString(date: diary.date)
     }
-    */
+    
+    private func dateToString(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.string(from: date)
+    }
+    
+    @IBAction func editButtonTapped(_ sender: UIButton ) {
+    }
+    @IBAction func deleteButtonTapped(_ sender: UIButton) {
+        guard let indexPath = self.indexPath else { return }
+        self.delegate?.didSelectDelete(indexPath: indexPath)
+        self.navigationController?.popViewController(animated: true)
 
+    }
 }
--- projects/DiaryApp/DiaryApp/ViewController.swift
+++ projects/DiaryApp/DiaryApp/ViewController.swift
@@ -92,7 +92,14 @@ extension ViewController: UICollectionViewDataSource {
 }
 
 extension ViewController: UICollectionViewDelegate {
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "DiaryDetailViewController") as? DiaryDetailViewController else { return }
+        let diary = self.diaryList[indexPath.row]
+        viewController.diary = diary
+        viewController.indexPath = indexPath
+        viewController.delegate = self
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
@@ -110,5 +117,12 @@ extension ViewController: WriteDiaryViewDeleage {
             $0.date.compare($1.date) == .orderedDescending
         }
         self.collectionView.reloadData()
+    }
+}
+
+extension ViewController: DiaryDetailViewDelegate {
+    func didSelectDelete(indexPath: IndexPath) {
+        self.diaryList.remove(at: indexPath.row)
+        self.collectionView.deleteItems(at: [indexPath])
     }
 }
