7d6a40b7ebaed8be71659e89715924b9591882c7
JuseobJang <wntjq68@gmail.com>
2021-10-15 00:36:38 +0900

    feat: Implement fetch COVID19 data using API & Alamofire
    Alamofire 라이브러리를 사용하여  API 를 통해 코로나19 관련 정보를 불러옴

--- projects/COVID19App/COVID19App/ViewController.swift
+++ projects/COVID19App/COVID19App/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Charts
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -15,9 +16,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var pieChartView: PieChartView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.fetchCovidOverview { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let data):
+                debugPrint("success : \(data)")
+            case .failure(let error):
+                debugPrint("failure : \(error.localizedDescription)")
+            }
+        }
     }
-
+    
+    func fetchCovidOverview(completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void) {
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = ["serviceKey": "8ytkocahpzLx1l79rMiHGemYCF5XgD26K"]
+        AF.request(url, method: .get, parameters: param)
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverview.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                case .failure(let error) :
+                    completionHandler(.failure(error))
+                }
+            }
+    }
 
 }
 
