8b324f946f8e1f5968316715a9c2d4c89a3aa7e1
JuseobJang <wntjq68@gmail.com>
2021-10-14 15:24:47 +0900

    feat: Implement CountDown UI Update
    타이머 카운트 다운에 따른 timer laberl ui 업데이트

--- projects/PomodoroApp/PomodoroApp/ViewController.swift
+++ projects/PomodoroApp/PomodoroApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 enum TimerStatus {
     case start
@@ -48,10 +49,17 @@ class ViewController: UIViewController {
             self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
             self.timer?.schedule(deadline: .now(), repeating: 1)
             self.timer?.setEventHandler(handler: { [weak self] in
-                self?.currentSeconds -= 1
-                debugPrint(self?.currentSeconds)
-                if self?.currentSeconds ?? 0 <= 0 {
-                    self?.stopTimer()
+                guard let self = self else { return }
+                self.currentSeconds -= 1
+                let hour = self.currentSeconds / 3600
+                let minutes = (self.currentSeconds % 3600) / 60
+                let seconds = (self.currentSeconds % 3600) % 60
+                self.timerLabel.text = String(format: "%02d:%02d:%02d", hour, minutes, seconds)
+                self.progressView.progress = Float(self.currentSeconds) / Float(self.duration)
+                
+                if self.currentSeconds <= 0 {
+                    self.stopTimer()
+                    AudioServicesPlaySystemSound(1005)
                 }
             })
             self.timer?.resume()
