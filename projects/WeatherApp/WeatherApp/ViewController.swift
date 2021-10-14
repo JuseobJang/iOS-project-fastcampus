58461b209574df956301c954e33ce952695a2099
JuseobJang <wntjq68@gmail.com>
2021-10-14 17:16:03 +0900

    feat: Implement get weather data using api
    API 를 사용하여 Weather정보를 가져오기 위해 Codable을 채택한 구조체 생성 및 JSONDecoder를 사용하여 decoding

--- projects/WeatherApp/WeatherApp/ViewController.swift
+++ projects/WeatherApp/WeatherApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weaherDescriptionLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func fetchWeatherButtonTapped(_ sender: Any) {
+        if let cityName = self.cityTextField.text {
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true)
+        }
+    }
+    
+    func getCurrentWeather(cityName: String){
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=9ed9a56f2a23c14f0a2c74fbeef94dc3") else { return }
+        
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            let decoder = JSONDecoder()
+            let weatherInfo = try? decoder.decode(WeatherInfo.self, from: data)
+            debugPrint(weatherInfo)
+        }.resume()
+    }
+    
 }
 
