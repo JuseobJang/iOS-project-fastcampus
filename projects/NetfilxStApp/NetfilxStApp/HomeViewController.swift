1556944e265b9fffbfe3b2e57d53d2bdf0f44f53
JuseobJang <wntjq68@gmail.com>
2021-10-16 14:13:54 +0900

    feat: Create NetflixStApp project
    1. 스토리 보드 대신 SnapKit을 이용하여 앱을 구현
    2. SceneDelegate의 scene() 메서드 정의 ( 초기 화면 )

---
+++ projects/NetfilxStApp/NetfilxStApp/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  NetfilxStApp
+//
+//  Created by seob_jj on 2021/10/16.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+    
+}
---
+++ projects/NetfilxStApp/NetfilxStApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetfilxStApp
+//
+//  Created by seob_jj on 2021/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
