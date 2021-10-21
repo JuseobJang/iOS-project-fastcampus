10ca9fd30fd8cad2106d5b4ad9e612fc79e2939e
JuseobJang <wntjq68@gmail.com>
2021-10-21 16:05:49 +0900

    feat: Implement Beer Struct
    1. API로 부터 받을 Response에서 필요한 값만 가져오는 Beer Struct생성
    2. 가져온 taglineString을 통해 #tag 형태의 해시태그 값으로 전환 출력

---
+++ projects/BreweryApp/BreweryApp/Beer.swift
@@ -0,0 +1,33 @@
+//
+//  Beer.swift
+//  BreweryApp
+//
+//  Created by seob_jj on 2021/10/21.
+//
+
+import Foundation
+
+struct Beer: Decodable {
+    let id: Int?
+    let name, taglineString, description, brewersTips, imageURL: String?
+    let foodParing: [String]?
+    
+    var tagLine: String {
+        let tags = taglineString?.components(separatedBy: ". ")
+        let hashtags = tags?.map{
+            "#" + $0.replacingOccurrences(of: " ", with: "")
+                .replacingOccurrences(of: ".", with: "")
+                .replacingOccurrences(of: ",", with: "")
+        }
+        return hashtags?.joined(separator: " ") ?? ""
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description
+        case taglineString = "tagline"
+        case imageURL = "image_url"
+        case brewersTips = "brewers_tips"
+        case foodParing = "food_paring"
+    }
+    
+}
