b1e85dd683518b8cf255035ac1e3f6dfb56cbc5f
JuseobJang <wntjq68@gmail.com>
2021-10-21 15:36:17 +0900

    feat: Configure initial UI setting
    1. SnapKit및 코드를 이용한 UI 설정을 위해 스토리보드 삭제 및 SceneDelegate 변경
    2. SnapKit, KingFisher package 다운로드

---
+++ projects/BreweryApp/BreweryApp/BeerListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BeerListViewController.swift
+//  BreweryApp
+//
+//  Created by seob_jj on 2021/10/21.
+//
+
+import UIKit
+
+
+class BeerListViewController: UITableViewController{
+    
+}
