ed6834a9542238f0b320f3dbdd0b5e7a36e1336c
JuseobJang <wntjq68@gmail.com>
2021-10-28 15:09:56 +0900

    feat: Create MyAssets peoject
    자산관리앱 프로젝트 시작

---
+++ projects/MyAssets/MyAssets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAssets
+//
+//  Created by seob_jj on 2021/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ projects/MyAssets/MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by seob_jj on 2021/10/28.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
