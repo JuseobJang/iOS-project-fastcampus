b142b14e75917b02c158a732c67130dc8bf9ad7a
JuseobJang <wntjq68@gmail.com>
2021-10-28 15:06:41 +0900

    feat: Change Content Detail View UI
    1. SwiftUI의 ZStack을 사용하여 해당 뷰의 UI를 구성함

--- projects/NetfilxStApp/NetfilxStApp/ContentDetailView.swift
+++ projects/NetfilxStApp/NetfilxStApp/ContentDetailView.swift
@@ -11,28 +11,35 @@ struct ContentDetailView: View {
     @State var item: Item?
     
     var body: some View {
-        VStack{
-            if let item = item {
-                Image(uiImage: item.image)
-                    .aspectRatio(contentMode: .fit)
+        ZStack{
+            Color.black.edgesIgnoringSafeArea(.all)
+            ZStack(alignment: .bottom){
+                if let item = item {
+                    Image(uiImage: item.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200)
+                    
+                    Text(item.description)
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .foregroundColor(.primary)
+                        .background(Color.primary.colorInvert().opacity(0.75))
+                    
+                } else {
+                    Color.white
+                }
                 
-                Text(item.description)
-                    .font(.caption)
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.center)
-                    .padding()
-                
-            } else {
-                Color.white
             }
-            
         }
     }
 }
 
 struct ContentDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        let item0 = Item(description: "룰루랄라", imageName: "poster0")
+        let item0 = Item(description: "내 이름은 장주섭 거꾸로하면 섭주장", imageName: "poster0")
         
         ContentDetailView(item: item0)
     }
