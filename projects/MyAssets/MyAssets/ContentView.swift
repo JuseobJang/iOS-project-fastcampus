0401a81460457bc144c008de829eb8d8b0b7dbab
JuseobJang <wntjq68@gmail.com>
2021-10-28 15:34:29 +0900

    feat: Implement Tab View
    SwiftUI를 사용하여 TabView 구현

--- projects/MyAssets/MyAssets/ContentView.swift
+++ projects/MyAssets/MyAssets/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .asset
+    
+    enum Tab {
+        case asset
+        case recommand
+        case alert
+        case setting
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            Color.white
+                .tabItem{
+                    Image(systemName: "dollarsign.circle.fill")
+                    Text("자산")
+                }
+                .tag(Tab.asset)
+            Color.blue
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "hand.thumbsup.fill")
+                    Text("추천")
+                }
+                .tag(Tab.recommand)
+            Color.yellow
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "bell.fill")
+                    Text("알림")
+                }
+                .tag(Tab.alert)
+            Color.red
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("설정")
+                }
+                .tag(Tab.setting)
+        }
     }
 }
 
