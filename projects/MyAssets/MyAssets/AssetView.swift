7b44f78afcc6ac044a13b4e0c8fc793b9a04aebb
JuseobJang <wntjq68@gmail.com>
2021-10-28 16:50:47 +0900

    feat: Implement AssetMenuGridView
    1. AssetMenu enum 정의
    2. 메뉴의 버튼 스타일 정의 
    3. 메뉴 그리드 뷰 정의
    4. 위에서 만든 그리드 뷰를 AssetView에 추가 및 네비게이션 바 적용

---
+++ projects/MyAssets/MyAssets/AssetView.swift
@@ -0,0 +1,30 @@
+//
+//  AssetView.swift
+//  MyAssets
+//
+//  Created by seob_jj on 2021/10/28.
+//
+
+import SwiftUI
+
+struct AssetView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 30) {
+                    Spacer()
+                    AssetMenuGridView()
+                    
+                }
+            }
+            .background(Color.gray.opacity(0.2))
+            .navigationBarWithButtonStyle("내 자산")
+        }
+    }
+}
+
+struct AssetView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetView()
+    }
+}
